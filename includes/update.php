024a0de6bf3ecc4dca2a4c81d41ba1e642337d3a
Richer Yang <fantasyworldidvtw@gmail.com>
2026-06-24 14:29:06 +0800

    全新安裝時不跑更新流程

--- includes/update.php
+++ includes/update.php
@@ -6,12 +6,14 @@ final class RY_WSI_update
 {
     public static function update()
     {
-        $now_version = RY_WSI::get_option('version');
+        $now_version = RY_WSI::get_option('version', '0.0.0');
 
-        if (false === $now_version) {
-            $now_version = '0.0.0';
-        }
         if (RY_WSI_VERSION === $now_version) {
+            return;
+        }
+
+        if ($now_version === '0.0.0') {
+            RY_WSI::update_option('version', RY_WSI_VERSION, true);
             return;
         }
 
